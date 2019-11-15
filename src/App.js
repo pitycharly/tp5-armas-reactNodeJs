216cf0a40db912c583e939b6e73bfa610e66848f
BuildTools <unconfigured@null.spigotmc.org>
2019-11-15 09:25:18 -0300

    make it better

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+// import "./bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ReporteArmas from "./components/ReporteArmas";
+import Arma from "./components/Arma";
+import ListadoArmas from "./components/ListadoArmas";
+
+
+class App extends Component {
+  state = {
+    armas: []
+  };
+
+  //! Cuando la aplicacion se carga
+  componentDidMount() {
+    const armasLS = localStorage.getItem("armas");
+    if (armasLS) {
+      this.setState({ armas: JSON.parse(armasLS) });
+    }
+  }
+
+  //! Cuando eliminamos o agregamos una nueva arma
+  componentDidUpdate() {
+    localStorage.setItem("armas", JSON.stringify(this.state.armas));
+  }
+
+  crearReporte = datos => {
+    //console.log(datos);
+    //copiar el state actual
+    const armas = [...this.state.armas, datos];
+
+    // agregar el nuevo state
+    this.setState({
+      armas
+    });
+  };
+
+  //! Elimina las armas del state
+
+  eliminarArma = id => {
+    // console.log(id);
+    //! tomar una copia del state
+    const armasActuales = [...this.state.armas];
+    const armas = armasActuales.filter(arma => arma.id !== id);
+
+    this.setState({ armas });
+  };
+
+  render() {
+    return (
+      <div className="container">
+
+        <ReporteArmas crearReporte={this.crearReporte} />
+
+        <ListadoArmas
+          armas={this.state.armas}
+          eliminarArma={this.eliminarArma}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
+
+//
+// function App() {
+//   const [armas, guardarArmas] = useState([]);
+//   const [recargarArmas, guardarRecargarArmas] = useState(true);
+//
+//   useEffect(()=>{
+//     if(recargarArmas) {
+//       const armasLS = localStorage.getItem("armas");
+//           if (armasLS) {
+//           guardarArmas({ armas: JSON.parse(armasLS) });
+//           }
+//
+//
+//   }
+//   guardarRecargarProductos(false);
+//   );
+//
+//
+//
+//
+//   return (
+//     <ReporteArmas/>
+//   );
+// }
+//
+// export default App;
